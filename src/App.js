225761a05c46f833bb14c46b9a5a352d0447b749
Alexander Volkov <agvolkov5@gmail.com>
2019-03-12 14:53:05 +0300

    tiny improvements
    enter handling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,201 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import formula_5_1 from './img/5-1.png';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+
+      this.smo_kinds = {
+          '0sr': {
+              title: 'Одноканальная СМО с отказами',
+              t_input_visible: false,
+          },
+          '0mr': {
+              title: 'Многоканальная СМО с отказами',
+              t_input_visible: false,
+          },
+          '0sl': {
+              title: 'Одноканальная СМО с ограниченной длиной очереди',
+              t_input_visible: false,
+          },
+          '0si': {
+              title: 'Одноканальная СМО с неограниченной очередью',
+              t_input_visible: false,
+          },
+          '0ml': {
+              title: 'Многоканальная СМО с ограниченной очередью',
+              t_input_visible: true,
+          },
+          '0mi': {
+              title: 'Многоканальная СМО с неограниченной очередью',
+              t_input_visible: false,
+          },
+          '0mll': {
+              title: 'Многоканальная СМО с ограниченной длиной очереди и ограниченным временем ожидания в очереди',
+              t_input_visible: true,
+          },
+          '1': {
+              title: 'n-канальная СМО замкнутого типа с m источниками заявок',
+              t_input_visible: false,
+          }
+      };
+
+      this.state = {
+          lambda_value: '',
+          mu_value: '',
+          n_value: '',
+          m_value: '',
+          t_value: '',
+          m_radio: 'queue length limit',
+          smo: this.smo_kinds['0si'],
+      };
+  }
+  getCurrentSMOKind = () => {
+      const m_radio = this.state.m_radio;
+      const n = this.state.n_value;
+      const m = this.state.m_value;
+      const t = this.state.t_value;
+
+      let code = 1 - (m_radio.toString() === 'queue length limit');
+      if (code === 0) {
+          if (n === '1' || n === '') {
+              code += 's'
+          } else {
+              code += 'm'
+          }
+
+          if (m === '0') {
+              code += 'r'
+          } else if (m === '∞' || m === '') {
+              code += 'i'
+          } else {
+              code += 'l'
+          }
+
+          if (code === '0ml' && t !== '∞' && t !== '') {
+              code += 'l'
+          }
+      }
+
+      const smo = this.smo_kinds[code];
+      this.setState({smo: smo}, () => {
+          console.log(this.state.smo.t_input_visible !== true);
+      });
+  };
+
+  n_handler = event => {
+    this.setState({n_value: event.target.value}, () => {
+        this.getCurrentSMOKind();
+    });
+  };
+  m_handler = event => {
+    this.setState({m_value: event.target.value}, () => {
+        this.getCurrentSMOKind();
+    });
+  };
+  t_handler = event => {
+    this.setState({t_value: event.target.value}, () => {
+        this.getCurrentSMOKind();
+    });
+  };
+  queueLengthLimitOn = event => {
+    this.setState({m_radio: 'queue length limit'}, () => {
+        this.getCurrentSMOKind();
+    });
+  };
+  numberOfResponseSources = event => {
+    this.setState({m_radio: 'number of response sources'}, () => {
+        this.getCurrentSMOKind();
+    });
+  };
+
   render() {
+    const t_visible_className = this.state.smo.t_input_visible === true ? 'option-item' : 'option-item hide';
+    console.log(t_visible_className);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="grid-container">
+          <div className="apps-title">Queueing Theory Solver</div>
+          <div className="tasks-kinds-wrapper">
+            <div>Уравнения Колмогорова</div>
+            <div>СМО</div>
+          </div>
+          <div className="options-container">
+              <div className="option-item">
+                  <div className="option-name">λ</div>
+                  <div className="equal-symbol">=</div>
+                  <input type="text"
+                        placeholder="lambda"
+                        value="0"
+                        onChange={this.n_handler}
+                  />
+              </div>
+              <div className="option-item">
+                  <div className="option-name">μ</div>
+                  <div className="equal-symbol">=</div>
+                  <input type="text"
+                         placeholder="mu"
+                         value="0"
+                         onChange={this.n_handler}
+                  />
+              </div>
+              <div className="flex-br"/>
+              <div className="option-item">
+                  <div className="option-name">n</div>
+                  <div className="equal-symbol">=</div>
+                  <input type="text"
+                         placeholder="1"
+                         value={this.state.n_value}
+                         onChange={this.n_handler}
+                  />
+              </div>
+              <div className="option-item">
+                  <div className="option-name">m</div>
+                  <div className="equal-symbol">=</div>
+                  <input type="text"
+                         placeholder="∞"
+                         value={this.state.m_value}
+                         onChange={this.m_handler}
+                  />
+              </div>
+              <div className="m-radio">
+                  <div className="radio-title">m означает:</div>
+                  <div>
+                      <input type="radio"
+                             id="constraint-for-queue-length-radio"
+                             name="m-definition"
+                             onChange={this.queueLengthLimitOn}
+                             checked={this.state.m_radio === 'queue length limit'}
+                      />
+                      <label htmlFor="constraint-for-queue-length-radio">ограничение длины очереди</label>
+                  </div>
+                  <div>
+                      <input type="radio"
+                             id="count-of-responses-radio"
+                             name="m-definition"
+                             onChange={this.numberOfResponseSources}
+                             checked={this.state.m_radio === 'number of response sources'}
+                      />
+                      <label htmlFor="count-of-responses-radio">число источников заявок</label>
+                  </div>
+              </div>
+              <div className={t_visible_className}>
+                  <div className="option-name">
+                      t<sub>ож</sub>
+                  </div>
+                  <div className="equal-symbol">=</div>
+                  <input type="text"
+                         placeholder="∞"
+                         value={this.state.t_value}
+                         onChange={this.t_handler}
+                  />
+              </div>
+          </div>
+          <div className="results-container">
+              <h1>{this.state.smo.title}</h1>
+              <img src={formula_5_1} height="70pt" alt="formula 5.1"/>
+          </div>
+        </div>
     );
   }
 }
